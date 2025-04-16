983a06b889efa4870c45522fbf59bbc7bc945496
Sayujya_rumsan <sayujya@rumsan.net>
2025-04-16 12:03:15 +0545

    toggle solved/ updated

--- tests/components/toggle.test.ts
+++ tests/components/toggle.test.ts
@@ -1,3 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test("", () => {});
+test("toggle theme from light to dark and back", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+  await page.pause();
+  const theme = await page.getByTestId("theme-container");
+  const btn = await page.getByRole("button", { name: "Toggle Theme" });
+
+  // Initially light
+  await expect(theme).toHaveClass(/bg-white/);
+
+  // Click dark
+  await btn.click();
+  await expect(theme).toHaveClass(/bg-gray-900/);
+
+  // Click light
+  await btn.click();
+  await expect(theme).toHaveClass(/bg-white/);
+});
