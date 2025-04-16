91b5dd3674274a22e08ffa15a2e2a1289ac4d3ac
Sayujya_rumsan <sayujya@rumsan.net>
2025-04-16 11:31:18 +0545

    toggle changes

--- tests/components/count.test.ts
+++ tests/components/count.test.ts
@@ -2,15 +2,16 @@ import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000");
+  await page.pause();
 });
 
 test("initial count is 0", async ({ page }) => {
-  const count = page.locator('[data-testid="count-value"]');
+  const count = await page.getByTestId("count-value");
   await expect(count).toHaveText("0");
 });
 
 test("clicking button increases count", async ({ page }) => {
-  const button = page.getByRole("button", { name: /increase/i });
+  const button = page.getByRole("button", { name: "Increase" });
   await button.click();
   const count = page.locator('[data-testid="count-value"]');
   await expect(count).toHaveText("1");
